854237ce339f55e3f1807927b62fd5bdbdcafeed
Ishita Singh <ishitasingh.2315@gmail.com>
2026-08-09 23:04:51 +0530

    Added UC1 display Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Hello World Application - Use Case 1
+ *
+ * @author Ishita Singh
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
