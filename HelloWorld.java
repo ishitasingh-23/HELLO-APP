f39038ffd58f8fd30d149adee5996a37a9022015
Ishita Singh <ishitasingh.2315@gmail.com>
2026-08-09 23:31:35 +0530

    Added UC5 enhanced for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,18 @@
 /**
- * Hello World Application - Use Case 4
+ * Hello World Application - Use Case 5
  *
  * @author Ishita Singh
- * @version 4.0
+ * @version 5.0
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
             for (String name : args) {
-                System.out.println("Hello " + name);
+                System.out.println("Hello, " + name + "!");
             }
         }
     }
